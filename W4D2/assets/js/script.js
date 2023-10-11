5b65db0cf7294ec9365f2b9fe0dc4c18607cb33d
Mammadtaghi <tu7k3zlya>
2023-10-11 07:43:25 +0400

    Modifing names

---
+++ W4D2/assets/js/script.js
@@ -0,0 +1,24 @@
+let a = 9;
+let b = 3;
+
+console.log(a**b);
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a-b);
+
+console.log(a++);
+console.log(b--);
+
+console.log(a+=1);
+console.log(b-=1);
+console.log(a*=b);
+console.log(a/=b);
+console.log(a==b);
+console.log(a!=b);
+console.log(b===a);
+console.log();
+console.log();
+console.log();
+console.log();
