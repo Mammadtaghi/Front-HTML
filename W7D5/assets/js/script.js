36b8b483be32a125b205197769bf54e0fb79a85c
Mammadtaghi <tu7k3zlya>
2023-11-03 13:58:20 +0400

    Week 7 Day 5 added

---
+++ W7D5/assets/js/script.js
@@ -0,0 +1,11 @@
+// Search Icon Function
+// HTML Stuff
+// const magnifyingGlass = document.querySelector('.fa-magnifying-glass')
+// magnifyingGlass.addEventListener('click',(e)=>{
+//     e.preventDefault()
+
+//     // Create a Div
+//     const searchModal = document.querySelector('.searchModal')
+//     searchModal.style.animation = 'modal 0.2s ease-in-out 0s 1 forwards'
+
+// })
